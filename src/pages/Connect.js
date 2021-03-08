63740fd4cb4d9e847dfee8748a458495964c666d
Kevin Driscoll <kpdriscoll6@gmail.com>
2021-03-08 12:57:54 -0700

    modified package.json for deployment

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -4,29 +4,42 @@ import React from 'react';
 
 export default function Connect() {
   return (
-  <div className="container-fluid" id="media">
+  <div className="container-fluid p-5" id="media">
   <div className="container">
   <div className="row">
     <div className="col-12 text-center">
       <h1>Connect</h1>
     </div>
   </div>
-  <form name="contact" method="POST" data-netlify="true">
+  <form name="contact" method="POST" action="/contact" data-netlify="true">
+    <div className="row">
+      <div className="col">
+      <p>
+        <label>First Name: <input type="text" name="firstName" placeholder="First Name"/></label>   
+      </p>
+      </div>
+      <div className="col">
+      <p>
+        <label>Last Name: <input type="text" name="lastName" placeholder="Last Name"/></label>   
+      </p>
+      </div>
+    </div>
+    <div className="row">
     <p>
-      <label>First Name: <input type="text" name="name" /></label>   
+      <label>Email: <input type="email" name="email" placeholder="Email"/></label>
     </p>
+    </div>
+    <div className="row">
     <p>
-      <label>Last Name: <input type="text" name="name" /></label>   
+      <label>Message: <textarea name="message" placeholder="Message"></textarea></label>
     </p>
-    <p>
-      <label>Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
+    </div>
+    <div className="row">
+      <div className="col-12 text-center">
+      <button type="submit" className="btn btn-secondary">Send</button>
+    </div>
+    </div>
+    
   </form>
   </div>
   </div>
